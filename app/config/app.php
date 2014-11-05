7aa36019e7c032db0cd2ca9f6f30720936b28481
Matt Perry <matthewperry@fas.harvard.edu>
2014-11-05 09:53:48 -0500

    database

--- app/config/app.php
+++ app/config/app.php
@@ -188,7 +188,8 @@ return array(
 		'URL'             => 'Illuminate\Support\Facades\URL',
 		'Validator'       => 'Illuminate\Support\Facades\Validator',
 		'View'            => 'Illuminate\Support\Facades\View',
-
+    'Pre'             =>  'Paste\Pre',
+    
 	),
 
 );
